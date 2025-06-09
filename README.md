024a25d4c4141c5f138c0a6ca042d7d7df55f26f
azoozs <azoozsm2@used.with.github.com>
2025-06-09 13:43:46 +0300

    Initial upload of all files

---
+++ README.md
@@ -0,0 +1,17 @@
+# datasets 
+This Dataset is helpful to bypass antibot captcha in https://claimfreecoins.io/
+
+https://github.com/Full-University-Projects/OCR_Captcha_Dataset
+
+
+
+## Note
+These files when this dataset was small about 282 folder and 1134 files (or 1134 row in SQL)
+```
+antibot.zip
+
+captcha_solver.sql
+
+upload_antibot.bat
+```
+
---
+++ upload_antibot.bat
@@ -0,0 +1,143 @@
+@echo off
+setlocal enabledelayedexpansion
+
+:: Batch 301-400
+for /L %%i in (301,1,400) do @git add "antibot_header_%%i" & if %%i==400 (git commit -m "Added antibot headers 301-400" & git push origin main)
+
+:: Batch 401-500
+for /L %%i in (401,1,500) do @git add "antibot_header_%%i" & if %%i==500 (git commit -m "Added antibot headers 401-500" & git push origin main)
+
+:: Batch 501-600
+for /L %%i in (501,1,600) do @git add "antibot_header_%%i" & if %%i==600 (git commit -m "Added antibot headers 501-600" & git push origin main)
+
+:: Batch 601-700
+for /L %%i in (601,1,700) do @git add "antibot_header_%%i" & if %%i==700 (git commit -m "Added antibot headers 601-700" & git push origin main)
+
+:: Batch 701-800
+for /L %%i in (701,1,800) do @git add "antibot_header_%%i" & if %%i==800 (git commit -m "Added antibot headers 701-800" & git push origin main)
+
+:: Batch 801-900
+for /L %%i in (801,1,900) do @git add "antibot_header_%%i" & if %%i==900 (git commit -m "Added antibot headers 801-900" & git push origin main)
+
+:: Batch 901-1000
+for /L %%i in (901,1,1000) do @git add "antibot_header_%%i" & if %%i==1000 (git commit -m "Added antibot headers 901-1000" & git push origin main)
+
+:: Batch 1001-1100
+for /L %%i in (1001,1,1100) do @git add "antibot_header_%%i" & if %%i==1100 (git commit -m "Added antibot headers 1001-1100" & git push origin main)
+
+:: Batch 1101-1200
+for /L %%i in (1101,1,1200) do @git add "antibot_header_%%i" & if %%i==1200 (git commit -m "Added antibot headers 1101-1200" & git push origin main)
+
+:: Batch 1201-1300
+for /L %%i in (1201,1,1300) do @git add "antibot_header_%%i" & if %%i==1300 (git commit -m "Added antibot headers 1201-1300" & git push origin main)
+
+:: Batch 1301-1400
+for /L %%i in (1301,1,1400) do @git add "antibot_header_%%i" & if %%i==1400 (git commit -m "Added antibot headers 1301-1400" & git push origin main)
+
+:: Batch 1401-1500
+for /L %%i in (1401,1,1500) do @git add "antibot_header_%%i" & if %%i==1500 (git commit -m "Added antibot headers 1401-1500" & git push origin main)
+
+:: Batch 1501-1600
+for /L %%i in (1501,1,1600) do @git add "antibot_header_%%i" & if %%i==1600 (git commit -m "Added antibot headers 1501-1600" & git push origin main)
+
+:: Batch 1601-1700
+for /L %%i in (1601,1,1700) do @git add "antibot_header_%%i" & if %%i==1700 (git commit -m "Added antibot headers 1601-1700" & git push origin main)
+
+:: Batch 1701-1800
+for /L %%i in (1701,1,1800) do @git add "antibot_header_%%i" & if %%i==1800 (git commit -m "Added antibot headers 1701-1800" & git push origin main)
+
+:: Batch 1801-1900
+for /L %%i in (1801,1,1900) do @git add "antibot_header_%%i" & if %%i==1900 (git commit -m "Added antibot headers 1801-1900" & git push origin main)
+
+:: Batch 1901-2000
+for /L %%i in (1901,1,2000) do @git add "antibot_header_%%i" & if %%i==2000 (git commit -m "Added antibot headers 1901-2000" & git push origin main)
+
+:: Batch 2001-2100
+for /L %%i in (2001,1,2100) do @git add "antibot_header_%%i" & if %%i==2100 (git commit -m "Added antibot headers 2001-2100" & git push origin main)
+
+:: Batch 2101-2200
+for /L %%i in (2101,1,2200) do @git add "antibot_header_%%i" & if %%i==2200 (git commit -m "Added antibot headers 2101-2200" & git push origin main)
+
+:: Batch 2201-2300
+for /L %%i in (2201,1,2300) do @git add "antibot_header_%%i" & if %%i==2300 (git commit -m "Added antibot headers 2201-2300" & git push origin main)
+
+:: Batch 2301-2400
+for /L %%i in (2301,1,2400) do @git add "antibot_header_%%i" & if %%i==2400 (git commit -m "Added antibot headers 2301-2400" & git push origin main)
+
+:: Batch 2401-2500
+for /L %%i in (2401,1,2500) do @git add "antibot_header_%%i" & if %%i==2500 (git commit -m "Added antibot headers 2401-2500" & git push origin main)
+
+:: Batch 2501-2600
+for /L %%i in (2501,1,2600) do @git add "antibot_header_%%i" & if %%i==2600 (git commit -m "Added antibot headers 2501-2600" & git push origin main)
+
+:: Batch 2601-2700
+for /L %%i in (2601,1,2700) do @git add "antibot_header_%%i" & if %%i==2700 (git commit -m "Added antibot headers 2601-2700" & git push origin main)
+
+:: Batch 2701-2800
+for /L %%i in (2701,1,2800) do @git add "antibot_header_%%i" & if %%i==2800 (git commit -m "Added antibot headers 2701-2800" & git push origin main)
+
+:: Batch 2801-2900
+for /L %%i in (2801,1,2900) do @git add "antibot_header_%%i" & if %%i==2900 (git commit -m "Added antibot headers 2801-2900" & git push origin main)
+
+:: Batch 2901-3000
+for /L %%i in (2901,1,3000) do @git add "antibot_header_%%i" & if %%i==3000 (git commit -m "Added antibot headers 2901-3000" & git push origin main)
+
+:: Batch 3001-3100
+for /L %%i in (3001,1,3100) do @git add "antibot_header_%%i" & if %%i==3100 (git commit -m "Added antibot headers 3001-3100" & git push origin main)
+
+:: Batch 3101-3200
+for /L %%i in (3101,1,3200) do @git add "antibot_header_%%i" & if %%i==3200 (git commit -m "Added antibot headers 3101-3200" & git push origin main)
+
+:: Batch 3201-3300
+for /L %%i in (3201,1,3300) do @git add "antibot_header_%%i" & if %%i==3300 (git commit -m "Added antibot headers 3201-3300" & git push origin main)
+
+:: Batch 3301-3400
+for /L %%i in (3301,1,3400) do @git add "antibot_header_%%i" & if %%i==3400 (git commit -m "Added antibot headers 3301-3400" & git push origin main)
+
+:: Batch 3401-3500
+for /L %%i in (3401,1,3500) do @git add "antibot_header_%%i" & if %%i==3500 (git commit -m "Added antibot headers 3401-3500" & git push origin main)
+
+:: Batch 3501-3600
+for /L %%i in (3501,1,3600) do @git add "antibot_header_%%i" & if %%i==3600 (git commit -m "Added antibot headers 3501-3600" & git push origin main)
+
+:: Batch 3601-3700
+for /L %%i in (3601,1,3700) do @git add "antibot_header_%%i" & if %%i==3700 (git commit -m "Added antibot headers 3601-3700" & git push origin main)
+
+:: Batch 3701-3800
+for /L %%i in (3701,1,3800) do @git add "antibot_header_%%i" & if %%i==3800 (git commit -m "Added antibot headers 3701-3800" & git push origin main)
+
+:: Batch 3801-3900
+for /L %%i in (3801,1,3900) do @git add "antibot_header_%%i" & if %%i==3900 (git commit -m "Added antibot headers 3801-3900" & git push origin main)
+
+:: Batch 3901-4000
+for /L %%i in (3901,1,4000) do @git add "antibot_header_%%i" & if %%i==4000 (git commit -m "Added antibot headers 3901-4000" & git push origin main)
+
+:: Batch 4001-4100
+for /L %%i in (4001,1,4100) do @git add "antibot_header_%%i" & if %%i==4100 (git commit -m "Added antibot headers 4001-4100" & git push origin main)
+
+:: Batch 4101-4200
+for /L %%i in (4101,1,4200) do @git add "antibot_header_%%i" & if %%i==4200 (git commit -m "Added antibot headers 4101-4200" & git push origin main)
+
+:: Batch 4201-4300
+for /L %%i in (4201,1,4300) do @git add "antibot_header_%%i" & if %%i==4300 (git commit -m "Added antibot headers 4201-4300" & git push origin main)
+
+:: Batch 4301-4400
+for /L %%i in (4301,1,4400) do @git add "antibot_header_%%i" & if %%i==4400 (git commit -m "Added antibot headers 4301-4400" & git push origin main)
+
+:: Batch 4401-4500
+for /L %%i in (4401,1,4500) do @git add "antibot_header_%%i" & if %%i==4500 (git commit -m "Added antibot headers 4401-4500" & git push origin main)
+
+:: Batch 4501-4600
+for /L %%i in (4501,1,4600) do @git add "antibot_header_%%i" & if %%i==4600 (git commit -m "Added antibot headers 4501-4600" & git push origin main)
+
+:: Batch 4601-4700
+for /L %%i in (4601,1,4700) do @git add "antibot_header_%%i" & if %%i==4700 (git commit -m "Added antibot headers 4601-4700" & git push origin main)
+
+:: Batch 4701-4800
+for /L %%i in (4701,1,4800) do @git add "antibot_header_%%i" & if %%i==4800 (git commit -m "Added antibot headers 4701-4800" & git push origin main)
+
+:: Batch 4801-4900
+for /L %%i in (4801,1,4900) do @git add "antibot_header_%%i" & if %%i==4900 (git commit -m "Added antibot headers 4801-4900" & git push origin main)
+
+:: Batch 4901-5000
+for /L %%i in (4901,1,5000) do @git add "antibot_header_%%i" & if %%i==5000 (git commit -m "Added antibot headers 4901-5000" & git push origin main)
